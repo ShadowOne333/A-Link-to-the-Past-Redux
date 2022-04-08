37560ebe9c973a74c8f84f7a73a51f85427dd8a8
ShadowOne333 <shadow_one666@hotmail.com>
2022-04-08 12:09:02 -0600

    Forgot to change a line

--- code/main.asm
+++ code/main.asm
@@ -97,7 +97,7 @@ incsrc text/dialogue2.asm		; 2nd dialogue table
 incsrc text/items.asm
 
 ; Credits text
-incsrc text/retranslation/credits_ret.asm
+incsrc text/retranslation/credits.asm
 
 ;****************************************
 ;	Visual changes
