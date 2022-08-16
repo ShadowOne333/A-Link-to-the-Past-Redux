c88f8d5ee527e26663a5c1f393599a15a90515cc
ShadowOne333 <shadow_one666@hotmail.com>
2022-08-16 13:32:43 -0600

    Fix the VC patches values

--- code/fixes/vc_patches.asm
+++ code/fixes/vc_patches.asm
@@ -13,6 +13,7 @@ org $07A50D	; 0x03A50D
 
 org $0CFE6E	; 0x067E6E
 	lda #$1511	; Original VC patch had #$1505
+	; Original game had $151F
 
 ;----------------------------------------
 ; Modifies immediate operands to ADC instructions in a routine that adjusts colors to gradually tint more towards white. IIRC this is used when the seal on Ganon's Tower is being broken by the Crystal Maidens. The tweaks put in place cause the flashing process to take about 7 times longer.
@@ -24,9 +25,9 @@ org $0CFE6E	; 0x067E6E
 org $0ED804	; 0x075804
 	adc #$0002	; Originally ADC #$000E
 org $0ED817	; 0x075817
-	adc #$0140	; Originally ADC #$01C0
+	adc #$0040	; Originally ADC #$01C0
 org $0ED82A	; 0x07582A
-	adc #$0008	; Originally ADC #$0038
+	adc #$0800	; Originally ADC #$3800
 
 ;----------------------------------------
 ; These modify the lower bytes of entries in a large table full of palettes. The first two reduce the red component, and the last change reduces the red and green components of a color. Which palettes? It would take more effort I'd rather not do at the moment to figure that out. Best I can say at the moment is that they are palettes meant for BG tiles in indoor parts of the game. Specifically, my notes mark them as "DungeonMain BG palettes (6 in each group)"
@@ -35,7 +36,11 @@ org $0ED82A	; 0x07582A
 ;000DE040: FF 57
 
 org $01BE03C	; 0x0DE03C
-	dw $7072,$959C,$57FF
+	db $70
+org $01BE03E	; 0x0DE03E
+	db $95
+org $01BE040	; 0x0DE040
+	db $57
 
 ;----------------------------------------
 
