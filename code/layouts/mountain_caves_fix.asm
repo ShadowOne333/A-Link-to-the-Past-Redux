6fc827be0d132f98decf5c40606bb17431927800
ShadowOne333 <shadow_one666@hotmail.com>
2025-11-25 10:14:49 -0700

    Fix camera issues when entering/exiting the fixed Mountain Caves

--- code/layouts/mountain_caves_fix.asm
+++ code/layouts/mountain_caves_fix.asm
@@ -15,38 +15,38 @@ org $02DDC8	; 0x015DC8
 
 org $02DEB6	; 0x015EB6
 ; Originally db $18 : skip 1 : db $0F
-	db $0F : skip 1 : db $18
+	;db $0F : skip 1 : db $18
 
 ; Change rooms Scroll Y values
 org $02DF53	; 0x015F53
 ; Originally $0308,$01E8
 	;  00FF, 00EF
-	dw $01E8,$0308
+	;dw $01E8,$0308
 
 ; Change rooms Y-Pos values
 org $02E08F	; 0x01608F
 ; Originally $0357,$0237
 	;  00FF, 00EF	<- Rooms
-	dw $0237,$0357
+	;dw $0237,$0357
 
 org $02E12D	; 0x01612D
 ; Originally db $CA : skip 1 : db $AA
-	db $AA : skip 1 : db $CA
+	;db $AA : skip 1 : db $CA
 
 ; Change rooms Center Y values
 org $02E1CB	; 0x0161CB
 ; Originally $0372,$0252
 	;  00FF, 00EF	<- Rooms
-	dw $0252,$0372
+	;dw $0252,$0372
 
 org $02E269	; 0x016269
 ; Originally db $D2 : skip 1 : db $B2
-	db $B2 : skip 1 : db $D2
+	;db $B2 : skip 1 : db $D2
 
 ; Change unknown value from both Exits
 org $02E2E8	; 0x0162E8
 ; Originally db $0B,$0A
-	db $0A,$0B
+	;db $0A,$0B
 
 ;-------------------------------------
 ; Change Death Mountain Entrances $1E <-> $1F
